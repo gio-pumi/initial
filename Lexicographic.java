35c64bd8c48c20b352f1582eec043b537663598a
gio-pumi <giopumi13@gmail.com>
2025-04-14 01:36:15 -0400

    arrays

--- Lexicographic.java
+++ Lexicographic.java
@@ -6,50 +6,58 @@ public class Lexicographic {
         Scanner scanner = new Scanner(System.in);
         //your code here
 
-        String firstString= scanner.nextLine();
+        String firstString = scanner.nextLine();
         String secondString = scanner.nextLine();
 
         String[] firstArray = firstString.split("");
         String[] secondArray = secondString.split("");
 
-        int firstWordLexicCount =0;
-        int secondWordLexicCount =0;
-
         int[] num1 = new int[firstArray.length];
-        int[] num2 = new int[firstArray.length];
+        int[] num2 = new int[secondArray.length];
 
-        String[] alphabet = {"-","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        if (firstArray.length > secondArray.length)
+            System.out.println("second");
+        else if (firstArray.length < secondArray.length)
+            System.out.println("first");
+        else {
+            int firstWordLexicCount = 0;
+            int secondWordLexicCount = 0;
 
-        for (int i = 0; i < firstArray.length; i++) {
-            for (int j= 1; j <= alphabet.length; j++) {
-                if(alphabet[j].equals(firstArray[j])){
-                    num1[i] = j;
-                    break;
+
+            String[] alphabet = {"-", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
+            for (int i = 0; i <= firstArray.length - 1; i++) {
+                for (int j = 1; j <= alphabet.length; j++) {
+                    if (firstArray[i].equals(alphabet[j])) {
+                        num1[i] = j;
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i <= num2.length - 1; i++) {
+                System.out.println(num1[i]);
+            }
+
+            for (int i = 0; i <= secondArray.length - 1; i++) {
+                for (int j = 1; j <= alphabet.length; j++) {
+                    if (secondArray[i].equals(alphabet[j])) {
+                        num2[i] = j;
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i <= num2.length - 1; i++) {
+                if (num2[i] > num1[i])
+                    System.out.println(num1[i]);
+                else if (num2[i] < num1[i]) {
+                    System.out.println(num1[i]);
+                } else
+                        System.out.println(num2[i]);
                 }
             }
         }
- /*
-        for (int i = 0; i < secondArray.length; i++) {
-            for (int j= 1; j <= alphabet.length; j++) {
-                if(secondArray[i].equals(alphabet[j])){
-                    num2[i] = j;
-                    break;
-                }
-            }
-        }
+    }
 
-        for (int i = 0; i < secondArray.length; i++) {
-            if(num1[i]<num2[i]){
-                System.out.println("first");
-                break;
-            }
-            else  if(num1[i]>num2[i]){
-                System.out.println("first");
-                 break;}
-            else
-                System.out.println("equal");
-*/
-        }
-
-}
