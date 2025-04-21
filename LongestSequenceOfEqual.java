9b59dacf61d5d2940f94280ba624073a6d2fc428
gio-pumi <giopumi13@gmail.com>
2025-04-20 23:48:39 -0400

    alldone 4.24.25

--- LongestSequenceOfEqual.java
+++ LongestSequenceOfEqual.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LongestSequenceOfEqual {
@@ -8,9 +9,27 @@ public class LongestSequenceOfEqual {
         Integer[] numbersList = new Integer[nNumber];
 
         int maxRepeated = 0;
+        int last = 0;
+
+        ArrayList<Integer> list = new ArrayList<>();
+
 
         for (int i = 0; i < nNumber; i++) {
             numbersList[i] = Integer.parseInt(scanner.nextLine());
+            if (numbersList[i] == last ) {
+               maxRepeated++;
+            }
+            else{
+                list.add (maxRepeated+1);
+                maxRepeated = 0;
+                last = numbersList[i];
+
+            }
         }
+
+        for (int i = 0; i < list.size(); i++) {
+            maxRepeated = Math.max(maxRepeated, list.get(i));
+        }
+        System.out.println(maxRepeated);
     }
 }
